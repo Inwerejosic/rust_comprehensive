4fb6f59884d5666ec96bbd357566b59fd22bc4c6
Inwerejosic <inwerejosic@protonmail.com>
2026-03-12 08:53:17 +0100

    Set to program well.

--- chapt-1/chapt-1-exercise-1/src/main.rs
+++ chapt-1/chapt-1-exercise-1/src/main.rs
@@ -1,5 +1,7 @@
-// fn main() {
-//     let x:i16 = 10;
-//     println!("x: {x}")
-// }
+fn interproduct(a: i32, b:i32, c:i32) -> i32 {
+    return a * b + b * c * a;
+}
 
+fn main(){
+    println!("result: {}", interproduct(12, 100, 28))
+}
