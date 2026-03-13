e45e4417da7235396f32e6b1b8fd8a6e61511b47
Inwerejosic <inwerejosic@protonmail.com>
2026-03-13 22:25:06 +0100

    Moving to tinering with the week array!

--- chapt-1/chapt-1-exercise-3/src/main.rs
+++ chapt-1/chapt-1-exercise-3/src/main.rs
@@ -12,7 +12,15 @@ fn main() {
     // let size = if x < 20 { "small" } else { "large" };
     // println!("number size: {}", size)
 
-    let mut a:[i8; 5] = [5, 4, 3, 2, 1];
-    a[2] = 0;
-    println!("a: {a:?}");
+    // Rust Arrays
+
+    // let mut a:[i8; 5] = [5, 4, 3, 2, 1];
+    // a[0] = 7;
+    // println!("a: {a:?}");
+
+    let week_days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+    let today = week_days[3];
+    let tomorrow = week_days[4];
+    let yesterday = week_days[2];
+    println!("Today's date is {today:#}, yesterday was {yesterday:#} and tomorrow will be {tomorrow:?}")
 }
