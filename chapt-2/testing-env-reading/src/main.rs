3ae86e76f608185ec5b71af24848fa5879ac569b
inwerejosic <inwerejosic@protonmail.com>
2026-07-01 16:07:08 +0100

    Commit_01_07: The code is now looking more idomatic.

--- chapt-2/testing-env-reading/src/main.rs
+++ chapt-2/testing-env-reading/src/main.rs
@@ -1,15 +1,13 @@
 use dotenvy::dotenv;
 
 const ANSI_GREEN: &str = "\x1b[32m";
-// const ANSI_RESET: &str = "\x1b[0m";
 
-fn main() {
+fn main() -> anyhow::Result<()> {
     dotenv().ok();
 
-    let address = match dotenvy::var("WALLET_ADDRESS") {
-        Ok(address) => address,
-        Err(e) => panic!("Error: {}", e),
-    };
+    let address = dotenvy::var("WALLET_ADDRESS")?;
 
     println!("The Wallet address is: {}{}", ANSI_GREEN, address);
+
+    Ok(())
 }
