3e180303191b77bf97004266467d8781ae274242
inwerejosic <inwerejosic@protonmail.com>
2026-07-03 14:08:42 +0100

    Commit_03_07: Working on Enums for proper understanding

--- chapt-2/my_enums/src/main.rs
+++ chapt-2/my_enums/src/main.rs
@@ -1,3 +1,22 @@
+enum AccountStatus {
+    Active,
+    Suspended { reason: String,},
+    Closed,
+}
+
+impl AccountStatus {
+    fn is_unsafe(&self) -> bool {
+        match self {
+            AccountStatus::Active => false,
+            AccountStatus::Suspended { .. } => true,
+            AccountStatus::Closed => true,
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let status = AccountStatus::Suspended {
+        reason: String::from("Payment overdue"),
+    };
+    println!("Is unsafe: {}", status.is_unsafe());
 }
